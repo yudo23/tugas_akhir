458c3290d57a31c30b8a6d0b40877ff2300e8667
yudo23 <yudo.dendy23@gmail.com>
2022-05-18 12:08:26 +0700

    Commit

--- src/com/company/SettingAbstact.java
+++ src/com/company/SettingAbstact.java
@@ -1,6 +1,6 @@
 package com.company;
 
-abstract class SettingAbstact {
+abstract class SettingAbstact{
     public abstract void splash_screen_open();
     public abstract String developer();
     public abstract String nama_toko();
